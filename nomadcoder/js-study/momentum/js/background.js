2b425766b69feead92ec03b8aae0b5da71323b8b
2018007983 <2018007983@hanyang.ac.kr>
2023-02-06 01:51:25 +0900

    Revert "make momentum"
    This reverts commit c98ab8cdceaa7905c03e3b8a159caac47a18b15b.

--- nomadcoder/js-study/momentum/js/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["seongae.jpg", "seungho.jpg", "jenny.jpg"];
-
-const chooseImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chooseImage}`;
-
-document.body.appendChild(bgImage);
