ae743ff3345e1d977b56a0ac3af054a6ed99baf4
2018007983 <2018007983@hanyang.ac.kr>
2023-03-15 13:10:20 +0900

    layout clone coding

---
+++ layout-clone/best-horror-scenes-page/src/App.js
@@ -0,0 +1,145 @@
+import "./scss/styles.scss";
+
+function App() {
+  return (
+    <>
+      <div className="left-box">
+        <div className="title">BEST HORROR SCENES</div>
+        <div className="text">
+          <div className="subtitle">
+            An ever growing collection featuring some of the best scenes in
+            horror.
+          </div>
+          <p>
+            “Best Horror Scenes” is a collection of scenes I feel are some of
+            the most affecting in horror. Some may be simple black cat scares,
+            others may be more subdued or nuanced. Many come from films that
+            aren't necessarily “horror” but have elements or threads of horror.
+          </p>
+          <div className="connect">
+            <span>Newsletter</span>
+            <span>Suggest a Scene</span>
+            <span>YouTube</span>
+            <span>Twitter</span>
+            <span>Contact</span>
+            <span>RSS</span>
+            <span>Letterboxd</span>
+          </div>
+          <div className="bottom-paragraph">
+            Best Horror Scenes is a love letter to the Horror film genre by
+            Brandon Durham. Most film clips contain major spoilers, and it is
+            recommended you don’t watch the clip if you plan on seeing the film.
+          </div>
+        </div>
+      </div>
+      <header>
+        Currently viewing <a href="/">everything</a> sorted by{" "}
+        <a href="/">random</a> .
+      </header>
+      <div className="right-box">
+        <div className="movie">
+          <div className="wrapper">
+            <div className="title">
+              <span>Blow Out (1981)</span>
+              <span>Directed by Brian De Palma</span>
+            </div>
+            <div className="media">this is media</div>
+            <div className="subscript">
+              <p>
+                Taking a slightly different approach here and sharing a great
+                scene from a thriller that isn’t necessarily a horror scene, but
+                rather a parody of one. The ghost of Hitchcock is more present
+                than ever here in Brian De Palma’s Blow Out, and it’s glorious.
+              </p>
+              <div>
+                <span>- B</span>
+                <span>My rating: 8/10</span>
+              </div>
+            </div>
+            <div className="bottom">
+              <span>No Comments</span>
+              <span>Leave a Comment</span>
+            </div>
+          </div>
+        </div>
+        <div className="movie">
+          <div className="wrapper">
+            <div className="title">
+              <span>Blow Out (1981)</span>
+              <span>Directed by Brian De Palma</span>
+            </div>
+            <div className="media">this is media</div>
+            <div className="subscript">
+              <p>
+                Taking a slightly different approach here and sharing a great
+                scene from a thriller that isn’t necessarily a horror scene, but
+                rather a parody of one. The ghost of Hitchcock is more present
+                than ever here in Brian De Palma’s Blow Out, and it’s glorious.
+              </p>
+              <div>
+                <span>- B</span>
+                <span>My rating: 8/10</span>
+              </div>
+            </div>
+            <div className="bottom">
+              <span>No Comments</span>
+              <span>Leave a Comment</span>
+            </div>
+          </div>
+        </div>
+        <div className="movie">
+          <div className="wrapper">
+            <div className="title">
+              <span>Blow Out (1981)</span>
+              <span>Directed by Brian De Palma</span>
+            </div>
+            <div className="media">this is media</div>
+            <div className="subscript">
+              <p>
+                Taking a slightly different approach here and sharing a great
+                scene from a thriller that isn’t necessarily a horror scene, but
+                rather a parody of one. The ghost of Hitchcock is more present
+                than ever here in Brian De Palma’s Blow Out, and it’s glorious.
+              </p>
+              <div>
+                <span>- B</span>
+                <span>My rating: 8/10</span>
+              </div>
+            </div>
+            <div className="bottom">
+              <span>No Comments</span>
+              <span>Leave a Comment</span>
+            </div>
+          </div>
+        </div>
+        <div className="movie">
+          <div className="wrapper">
+            <div className="title">
+              <span>Blow Out (1981)</span>
+              <span>Directed by Brian De Palma</span>
+            </div>
+            <div className="media">this is media</div>
+            <div className="subscript">
+              <p>
+                Taking a slightly different approach here and sharing a great
+                scene from a thriller that isn’t necessarily a horror scene, but
+                rather a parody of one. The ghost of Hitchcock is more present
+                than ever here in Brian De Palma’s Blow Out, and it’s glorious.
+              </p>
+              <div>
+                <span>- B</span>
+                <span>My rating: 8/10</span>
+              </div>
+            </div>
+            <div className="bottom">
+              <span>No Comments</span>
+              <span>Leave a Comment</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
