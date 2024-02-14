d951c114de016174f238d8555cfff80039f29883
abc4571998 <abc4571998@naver.com>
2024-02-14 15:41:56 +0900

    Feat: next 시작 & Route & Layout

---
+++ nextjs14/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../../components/navigation";
+
+const page = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ nextjs14/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from "../../components/navigation";
+
+export default function page() {
+  return (
+    <div>
+      <h2>i'm about us page</h2>
+    </div>
+  );
+}
